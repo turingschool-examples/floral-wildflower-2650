7b87b64c6dbf970ad15c9ac38b97878a0c77b53e
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-04-02 16:25:40 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+@recipe_1 = Recipe.create!(name: "Rice", complexity: 1, genre: "Rice Genre")
+@ingredient_1 = @recipe_1.ingredients.create!(name: "Beef", cost: 40)
+@ingredient_2 = @recipe_1.ingredients.create!(name: "Chicken", cost: 20)
