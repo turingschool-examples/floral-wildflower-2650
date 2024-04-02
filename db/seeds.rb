8c3aa37e0f8f4bc4ed9bf75921e46c0efc696fde
Grant Davis <grantdavis303@gmail.com>
2024-04-02 07:09:44 -0600

    User Story 2 Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,21 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# Recipes will have a name, complexity and genre. For example Pasta, 2 (integer), Italian.
 
 # Ingredients will have a name and a cost stored as an integer.
-
 print "Creating Ingredients"
 sleep(1)
 Ingredient.create!(name: "Ground Beef", cost: 6)
 Ingredient.create!(name: "Onion", cost: 2)
 Ingredient.create!(name: "Red Bell Pepper", cost: 1)
-Ingredient.create!(name: "Salt", cost: 2)
+Ingredient.create!(name: "Garlic Salt", cost: 2)
 Ingredient.create!(name: "Bread", cost: 3)
+Ingredient.create!(name: "Butter", cost: 4)
+puts " - Done"
+
+# Recipes will have a name, complexity and genre. For example Pasta, 2 (integer), Italian.
+print "Creating Recipes"
+sleep(1)
+Recipe.create!(name: "Garlic Bread", complexity: 1, genre: "Italian")
+Recipe.create!(name: "Ground Beef Stir Fry", complexity: 1, genre: "Asian")
 puts " - Done"
---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe "recipes show page", type: :feature do
+
+    # User Story 2
+    it "recipes show page lists name, complexity, and genre" do
+        @recipe = Recipe.create!(name: "Ground Beef Stir Fry", complexity: 1, genre: "Asian")       
+        @recipe.ingredients.create!(name: "Ground Beef", cost: 6)
+        @recipe.ingredients.create!(name: "Onion", cost: 2)
+        @recipe.ingredients.create!(name: "Red Bell Pepper", cost: 1)
+        
+        visit "/recipes/#{@recipe.id}"
+        save_and_open_page
+
+        expect(page).to have_content ("#{@recipe.name}")
+       
+        within "#recipe_info" do
+            expect(page).to have_content ("#{@recipe.complexity}")
+            expect(page).to have_content ("#{@recipe.genre}")
+        end
+
+        within "#recipe_ingredients" do
+            expect(page).to have_content ("Ground Beef")
+            expect(page).to have_content ("Onion")
+            expect(page).to have_content ("Red Bell Pepper")
+        end
+    end
+
+    # User Story 3
+    it "recipes show page lists total cost of recipe" do
+        @recipe = Recipe.create!(name: "Ground Beef Stir Fry", complexity: 1, genre: "Asian")       
+        @recipe.ingredients.create!(name: "Ground Beef", cost: 6)
+        @recipe.ingredients.create!(name: "Onion", cost: 2)
+        @recipe.ingredients.create!(name: "Red Bell Pepper", cost: 1)
+
+        visit "/recipes/#{@recipe.id}"
+        save_and_open_page
+
+        expect(page).to have_content ("#{@recipe.total_cost}")
+        expect(@recipe.total_cost).to eq(9)
+    end
+
+end
