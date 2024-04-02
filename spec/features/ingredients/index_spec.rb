565692c8f9c24aed32154e94f4a24aba9e5bbe27
lance butler <lancej2004@gmail.com>
2024-04-02 16:46:46 -0600

    Test: US 1, list of all the ingredients and attributes

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Ingredients Index Page" do
+  before(:each) do
+    @ingredient_1 = Ingredient.create!(name: "BBQ", cost: 2)
+    @ingredient_1 = Ingredient.create!(name: "Chicken", cost: 12)
+  end
+  
+  describe 'User Story 1' do
+    it "list of all the ingredients and attributes" do
+      visit "/ingredients"
+
+      expect(page).to have_content("BBQ")
+      expect(page).to have_content("Chicken")
+      expect(page).to have_content(2)
+      expect(page).to have_content(12)
+    end
+  end
+end
