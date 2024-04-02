715c0706e1bd87efd3fb1440b800215db14f910c
Grant Davis <grantdavis303@gmail.com>
2024-04-02 06:20:35 -0600

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Recipes will have a name, complexity and genre. For example Pasta, 2 (integer), Italian.
+
+# Ingredients will have a name and a cost stored as an integer.
+
+print "Creating Ingredients"
+sleep(1)
+Ingredient.create!(name: "Ground Beef", cost: 6)
+Ingredient.create!(name: "Onion", cost: 2)
+Ingredient.create!(name: "Red Bell Pepper", cost: 1)
+Ingredient.create!(name: "Salt", cost: 2)
+Ingredient.create!(name: "Bread", cost: 3)
+puts " - Done"
