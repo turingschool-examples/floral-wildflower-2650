f5a1382422fa538d6b641ba9f789e0547af1a162
Mel Langhoff <mlanghoff@uwalumni.com>
2024-04-02 15:37:51 -0600

    US 2 complete

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Ingredients Index Page' do
+RSpec.describe 'Recipe Show Page' do
   before :each do
     @spaghetti_bolognese = Recipe.create!(name: "Spaghetti Bolognese", complexity: 3, genre: "Italian")
     @tacos = Recipe.create!(name: "Tacos", complexity: 4, genre: "Mexican")
@@ -23,17 +23,21 @@ RSpec.describe 'Ingredients Index Page' do
     RecipeIngredient.create!(recipe: @chicken_curry, ingredient: @garlic)
   end
 
-  describe 'User story 1' do
-    it 'displays all ingredients with their name and cost' do
-      visit "/ingredients"
-      
-      save_and_open_page
+  describe 'User Story 2' do
+    it 'has a recipes complexity, name, and genre' do
+      visit "/recipes/#{@spaghetti_bolognese.id}"
+save_and_open_page
+      expect(page).to have_content(@spaghetti_bolognese.name)
+      expect(page).to have_content(@spaghetti_bolognese.complexity)
+      expect(page).to have_content(@spaghetti_bolognese.genre) 
+    end
+
+    it 'lists the names of all the ingredients for the recipe' do
+      visit "/recipes/#{@spaghetti_bolognese.id}"
+
       expect(page).to have_content(@tomato.name)
-      expect(page).to have_content(@tomato.cost)
       expect(page).to have_content(@onion.name)
-      expect(page).to have_content(@onion.cost)
-      expect(page).to have_content(@garlic.name)
-      expect(page).to have_content(@garlic.cost)
+      expect(page).to have_content(@garlic.name) 
     end
   end
 end
