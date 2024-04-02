391b5d6a8d8acf70cf99e7f9f406fc8edc482d13
Grant Davis <grantdavis303@gmail.com>
2024-04-02 08:37:50 -0600

    Update Spec Tests

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -4,33 +4,32 @@ RSpec.describe "ingredients index page", type: :feature do
     
     # User Story 1
     it "ingredients index page lists name and cost" do
-        ing_1 = Ingredient.create!(name: "Ground Beef", cost: 6)
-        ing_2 = Ingredient.create!(name: "Onion", cost: 2)
-        ing_3 = Ingredient.create!(name: "Red Bell Pepper", cost: 1)
+        @ing_1 = Ingredient.create!(name: "Ground Beef", cost: 6)
+        @ing_2 = Ingredient.create!(name: "Onion", cost: 2)
+        @ing_3 = Ingredient.create!(name: "Red Bell Pepper", cost: 1)
 
         visit "/ingredients"
         #save_and_open_page
 
-        expect(page).to have_content ("Ground Beef")
-        expect(page).to have_content ("6")
-        expect(page).to have_content ("Onion")
-        expect(page).to have_content ("2")
-        expect(page).to have_content ("Bell Pepper")
-        expect(page).to have_content ("1")
+        expect(page).to have_content (@ing_1.name)
+        expect(page).to have_content (@ing_1.cost)
+        expect(page).to have_content (@ing_2.name)
+        expect(page).to have_content (@ing_2.cost)
+        expect(page).to have_content (@ing_3.name)
+        expect(page).to have_content (@ing_3.cost)
     end
     
     # Extension 1
-    describe "class methods" do
-        it '#sort_alphebtically' do
-            @ing_1 = Ingredient.create!(name: "Red Bell Pepper", cost: 1)        
-            @ing_2 = Ingredient.create!(name: "Ground Beef", cost: 6)
-            @ing_3 = Ingredient.create!(name: "Onion", cost: 2)
+    it 'lists ingredients in alphabetical order' do
+        @ing_1 = Ingredient.create!(name: "Red Bell Pepper", cost: 1)        
+        @ing_2 = Ingredient.create!(name: "Ground Beef", cost: 6)
+        @ing_3 = Ingredient.create!(name: "Onion", cost: 2)
 
-            visit "/ingredients"
-            save_and_open_page
+        visit "/ingredients"
+        #save_and_open_page
 
-            expect(@ing_2.name).to appear_before(@ing_3.name)
-            expect(@ing_3.name).to appear_before(@ing_1.name)
-        end
+        expect(@ing_2.name).to appear_before(@ing_3.name)
+        expect(@ing_3.name).to appear_before(@ing_1.name)
     end
+
 end
